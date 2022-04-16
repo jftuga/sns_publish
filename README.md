ee63fecc2712576de341f68714d412e11e2d9207
John Taylor <jftuga@users.noreply.github.com>
2022-04-15 20:34:53 -0400

    add -f cmd-line option

--- README.md
+++ README.md
@@ -1,14 +1,29 @@
 # sns_publish
 command line tool to send a message to all AWS SNS topic subscribers
 
+You must supply a subject, message (or a file) and a topic ARN.
+* You can optionally pass -p to use a profile other than the default.
+* You can optionally pass -f to publish a file (instead of a message with -m).
+
+
 # usage:
 ```
-You must supply a subject, message and a topic ARN.
+Usage: sns_publish -p PROFILE -s SUBJECT [-m MESSAGE|-f FILE] -t TOPIC-ARN
 
-Usage: sns_publish -p PROFILE -s SUBJECT -m MESSAGE -t TOPIC-ARN
+  -f string
+    	Send a file (instead of a message with -m)
+  -m string
+    	The message to send to the topic subscribers; surround in quotes
+  -p string
+    	Profile name to use; optional
+  -s string
+    	The message subject; surround in quotes
+  -t string
+    	The SNS topic ARN; starts with 'arn:'
+
+
 ```
 
 # adopted from:
 * https://docs.aws.amazon.com/sdk-for-go/v1/developer-guide/sns-example-publish.html
 * https://github.com/awsdocs/aws-doc-sdk-examples/blob/master/go/example_code/sns/SnsPublish.go
-
--- sns_publish.go
+++ sns_publish.go
@@ -22,24 +22,34 @@ import (
 	"os"
 )
 
-const pgmVersion string = "1.0.4"
+const pgmVersion string = "1.1.0"
 const pgmUrl string = "https://github.com/jftuga/sns_publish"
+const maxMsgSize int = 262144
 
 func main() {
-	msgPtr := flag.String("m", "", "The message to send to the subscribed users of the topic")
-	subjectPtr := flag.String("s", "", "The message subject")
-	topicPtr := flag.String("t", "", "The ARN of the topic to which the user subscribes")
-	profilePtr := flag.String("p", "", "Profile name to use")
+	msgPtr := flag.String("m", "", "The message to send to the topic subscribers; surround in quotes")
+	subjectPtr := flag.String("s", "", "The message subject; surround in quotes")
+	topicPtr := flag.String("t", "", "The SNS topic ARN; starts with 'arn:'")
+	profilePtr := flag.String("p", "", "Profile name to use; optional")
+	filenamePtr := flag.String("f", "", "Send a file (instead of a message with -m)")
 	flag.Parse()
 
-	if *msgPtr == "" || *topicPtr == "" {
+	if (*msgPtr == "" && *filenamePtr == "") || *topicPtr == "" {
 		fmt.Printf("sns_publish, v%s\n", pgmVersion)
 		fmt.Println(pgmUrl)
 		fmt.Println("")
 		fmt.Println("You must supply a subject, message and a topic ARN.")
 		fmt.Println("You can optionally pass -p to use a profile other than the default.")
+		fmt.Println("You can optionally pass -f to publish a file (instead of a message with -m).")
 		fmt.Println("")
-		fmt.Println("Usage: sns_publish -p PROFILE -s SUBJECT -m MESSAGE -t TOPIC-ARN")
+		fmt.Println("Usage: sns_publish -p PROFILE -s SUBJECT [-m MESSAGE|-f FILE] -t TOPIC-ARN")
+		os.Exit(1)
+	}
+
+	if len(*msgPtr) > 0 && len(*filenamePtr) > 0 {
+		fmt.Println()
+		fmt.Println("Invalid options: -m and -f are mutually exclusive.")
+		fmt.Println()
 		os.Exit(1)
 	}
 
@@ -47,7 +57,7 @@ func main() {
 	if len(slots) != 6 {
 		fmt.Println()
 		fmt.Printf("Invalid TOPIC-ARN, %s\n", *topicPtr)
-		fmt.Println("This should contain 6 colon-delimited fields.")
+		fmt.Println("This should contain 6 colon-delimited fields and start with 'arn:'")
 		fmt.Println()
 		os.Exit(1)
 	}
@@ -66,10 +76,39 @@ func main() {
 		os.Exit(1)
 	}
 
+	var message *string
+
+	if len(*msgPtr) > 0 {
+		message = msgPtr
+	} else {
+		info, err := os.Stat(*filenamePtr)
+		if err != nil {
+			fmt.Println()
+			fmt.Println(err.Error())
+			fmt.Println()
+			os.Exit(1)
+		}
+		if info.Size() > 256*1024 {
+			fmt.Println()
+			fmt.Printf("Error: file size must be less then %d bytes\n", maxMsgSize)
+			fmt.Println()
+			os.Exit(1)
+		}
+		content, err := os.ReadFile(*filenamePtr)
+		if err != nil {
+			fmt.Println()
+			fmt.Println(err.Error())
+			fmt.Println()
+			os.Exit(1)
+		}
+		m := string(content)
+		message = &m
+	}
+
 	svc := sns.New(sess)
 	result, err := svc.Publish(&sns.PublishInput{
 		Subject:  subjectPtr,
-		Message:  msgPtr,
+		Message:  message,
 		TopicArn: topicPtr,
 	})
 	if err != nil {
